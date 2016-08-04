836228287169adc3a825b71f40364f3f2d5fde57
Di Zhu <zhudi0907@hotmail.com>
2016-08-04 18:03:12 -0400

    solve card bugs and changed part of css style for test on Firefox.

--- js/materialize.js
+++ js/materialize.js
@@ -3564,19 +3564,14 @@ $(document).ready(function(){
       if ($(this).find('> .card-reveal').length) {
         if ($(e.target).is($('.card-reveal .card-title')) || $(e.target).is($('.card-reveal .card-title i'))) {
           // Make Reveal animate down and display none
-          $(this).find('.card-reveal').velocity(
-            {translateY: 0}, {
-              duration: 225,
-              queue: false,
-              easing: 'easeInOutQuad',
-              complete: function() { $(this).css({ display: 'none'}); }
-            }
-          );
+          $(this).find('.card-reveal').fadeOut( "slow" );
+          $(this).find('.card-reveal').siblings().css('visibility', 'visible');
         }
         else if ($(e.target).is($('.card .activator')) ||
                  $(e.target).is($('.card .activator i')) ) {
           //$(e.target).closest('.card').css('overflow', 'hidden');
-          $(this).find('.card-reveal').css({ display: 'block'}).velocity("stop", false).velocity({translateY: '-100%'}, {duration: 300, queue: false, easing: 'easeInOutQuad'});
+        	$(this).find('.card-reveal').siblings().css('visibility', 'hidden');
+          $(this).find('.card-reveal').fadeIn( "slow" );
         }
       }
 
